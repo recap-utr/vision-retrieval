a5fdbd4331be8c035972d13e148f3ac68e934885
Kilian Bartz <kibartz@proton.me>
2025-03-17 11:06:22 +0000

    fixed treemaps

--- vis/treemaps.py
+++ vis/treemaps.py
@@ -2,54 +2,83 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import arguebuf as ab
 from PIL import Image
-from pathlib import Path
-import io
-from util import fig2img
-
-colors = {"Attack": "#e6194B", "Support": "#3cb44b"}
+from util import fig2img, ColorNode
 
 
-def get_treemap_rects(tree, x, y, width, height, horizontal, depth=0) -> list:
-    parts = len(tree["children"])
-    if parts == 0:
-        return (
-            [
-                {
-                    "x": x,
-                    "y": y,
-                    "width": width,
-                    "height": height,
-                    "depth": depth,
-                    "label": tree["label"],
-                }
-            ]
-            if "label" in tree
-            else []
-        )
-    if "label" in tree:
+class Node:
+
+    children: list["Node"] = []
+    neighbors: list[ColorNode] = []
+    node: ab.AbstractNode
+    color: ColorNode
+    parent: "Node | None"
+    is_dummy: bool
+
+    def __init__(
+        self,
+        node: ab.AbstractNode,
+        parent: "Node | None" = None,
+        grandparent: "Node | None" = None,
+        is_dummy: bool = False,
+    ) -> None:
+        self.node = node
+        self.parent = parent
+        self.neighbors = [parent.color] if (parent and not parent.is_dummy) else []
+        if grandparent and not grandparent.is_dummy:
+            self.neighbors.append(grandparent.color)
+        self.color = ColorNode(node, self.neighbors)
+        self.is_dummy = is_dummy
+
+    def set_children(self, children: list["Node"]):
+        self.children = children
+
+    def __str__(self):
+        s = f"[dummy: {self.is_dummy} {self.color.get_color()}]. Children: {len(self.children)}"
+        return s
+
+
+class Rectangle:
+
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y}) - ({self.width}, {self.height}): {self.color}"
+
+
+def get_treemap_rects(
+    tree_root: Node, x, y, width, height, horizontal
+) -> list[Rectangle]:
+    parts = len(tree_root.children)
+    # no children
+    if parts == 0 and not tree_root.is_dummy:
+        return [Rectangle(x, y, width, height, tree_root.color.get_color())]
+    # is scheme node
+    if parts == 0 and tree_root.is_dummy:
+        return []
+
+    next_horizontal = horizontal
+    if not tree_root.is_dummy:
         own_width = width if horizontal else 0.1 * width
         own_height = 0.1 * height if horizontal else height
         children_width = width / parts if horizontal else 0.9 * width
         children_height = height * 0.9 if horizontal else height / parts
-        res = [
-            {
-                "x": x,
-                "y": y,
-                "width": own_width,
-                "height": own_height,
-                "depth": depth,
-                "label": tree["label"],
-            }
-        ]
+        res = [Rectangle(x, y, own_width, own_height, tree_root.color.get_color())]
         if horizontal:
             y += own_height
         else:
             x += own_width
+        next_horizontal = not horizontal
+    # is not scheme node: node itself will not be drawn
     else:
+        res = []
         children_width = width / parts if horizontal else width
         children_height = height / parts if not horizontal else height
-        res = []
-    for i, child in enumerate(tree["children"]):
+    for i, child in enumerate(tree_root.children):
         if horizontal:
             child_x = x + i * children_width
             child_y = y
@@ -62,8 +91,7 @@ def get_treemap_rects(tree, x, y, width, height, horizontal, depth=0) -> list:
             child_y,
             children_width,
             children_height,
-            not horizontal,
-            depth + 1,
+            next_horizontal,
         )
     return res
 
@@ -81,20 +109,24 @@ def _get_children(graph: ab.Graph, node: ab.AbstractNode):
     return res
 
 
-def build_tree(graph: ab.Graph, root: ab.AbstractNode):
-    if not isinstance(root, ab.SchemeNode):
-        return {
-            "children": [
-                build_tree(graph, child) for child in _get_children(graph, root)
-            ]
-        }
-    if len(graph.incoming_nodes(root)) == 0:
-        return {"id": root.id, "label": root.label, "children": []}
-    return {
-        "id": root.id,
-        "label": root.label,
-        "children": [build_tree(graph, child) for child in _get_children(graph, root)],
-    }
+def build_tree(
+    graph: ab.Graph,
+    root: ab.AbstractNode,
+    parent: Node | None = None,
+    grandparent: Node | None = None,
+) -> Node:
+    node = Node(
+        root, parent, grandparent, is_dummy=(not isinstance(root, ab.SchemeNode))
+    )
+    if len(_get_children(graph, root)) > 0:
+        abstract_children = _get_children(graph, root)
+        children = [build_tree(graph, abstract_children[0], node, parent)]
+        previous_child = children[-1]
+        for child in abstract_children[1:]:
+            children.append(build_tree(graph, child, node, previous_child))
+            previous_child = children[-1]
+        node.set_children(children)
+    return node
 
 
 def visualize_treemap(
@@ -120,14 +152,14 @@ def visualize_treemap_inmem(
     tree = build_tree(graph, root)
 
     # get treemap rects
-    rects = get_treemap_rects(tree, 0, 0, width, height, True)
+    rects = get_treemap_rects(tree, 0, 0, width, height, False)
 
     # create png
     return fig2img(draw_treemap(rects, height, width, dpi=dpi))
 
 
-def draw_treemap(rects, height, width, dpi: int):
-    fig = plt.figure(figsize=(width, height), dpi=100)
+def draw_treemap(rects: list[Rectangle], height, width, dpi: int):
+    fig = plt.figure(figsize=(width, height), dpi=dpi)
 
     # Create axes that fill the entire figure
     ax = fig.add_axes((0, 0, 1, 1))
@@ -137,17 +169,13 @@ def draw_treemap(rects, height, width, dpi: int):
     ax.axis("off")  # Remove axis labeling
     ax.margins(0)  # Remove padding
     for rect in rects:
-        if "label" not in rect and rect["label"] != None:
-            continue
         ax.add_patch(
             patches.Rectangle(
-                (rect["x"], rect["y"]),
-                rect["width"],
-                rect["height"],
+                (rect.x, rect.y),
+                rect.width,
+                rect.height,
                 fill=True,
-                facecolor=colors.get(rect["label"], "white"),
-                linewidth=1,
-                edgecolor="black",
+                facecolor=rect.color,
             )
         )
 
