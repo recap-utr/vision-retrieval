f9ea4f39998f5729bda197399011f502f898e0f7
Kilian Bartz <kibartz@proton.me>
2025-03-24 10:13:56 +0000

    eval-oai/make_training_dataset cli

--- oai-finetuning/make_training_dataset.py
+++ oai-finetuning/make_training_dataset.py
@@ -1,7 +1,4 @@
-VIS = "space reclaiming icicle chart"
 IMG_PLACEHOLDER = "data:image/png;base64,"
-NUM_SAMPPLES = 500
-IMAGES_PER_SAMPLE = 2
 
 import base64
 import jsonlines
@@ -12,6 +9,11 @@ from torchvision import transforms
 import io
 from tqdm import tqdm
 import shutil
+import typer
+from pathlib import Path
+from typing_extensions import Annotated
+
+app = typer.Typer()
 
 
 def image_to_base64(image) -> str:
@@ -29,19 +31,19 @@ contrast_transforms = transforms.Compose(
     ]
 )
 
-def create_contrastive_views(image_path):
-    original_image = Image.open(image_path).convert('RGB')
+
+def create_contrastive_views(image_path, images_per_sample):
+    original_image = Image.open(image_path).convert("RGB")
     transform = contrast_transforms
-    
+
     contrastive_views = []
-    for _ in range(IMAGES_PER_SAMPLE):
+    for _ in range(images_per_sample):
         contrastive_views.append(transform(original_image))
-    
+
     return contrastive_views
-                                                            
 
 
-def build_sample(images: list, same: bool):
+def build_sample(images: list, same: bool, visualization: str):
     d = {
         "messages": [
             {
@@ -50,7 +52,7 @@ def build_sample(images: list, same: bool):
             },
             {
                 "role": "user",
-                "content": f"Take a look at the following images in {VIS} visualization. Are they visualizations of the same argument graph or do they describe different graphs? Answer only with 'same' or 'different'.",
+                "content": f"Take a look at the following images in {visualization} visualization. Are they visualizations of the same argument graph or do they describe different graphs? Answer only with 'same' or 'different'.",
             },
         ]
     }
@@ -74,54 +76,75 @@ def build_sample(images: list, same: bool):
     return d
 
 
-if __name__ == "__main__":
-    folder_path = "data/arg_finetune_logical_srip/srip2/train"
-    output_path = "data/oai-finetuning/"
-    if os.path.exists(output_path):
-        shutil.rmtree(output_path)
-
+@app.command()
+def create_training_dataset(
+    input_folder: Path,
+    output_folder: Path,
+    visualization: Annotated[
+        str,
+        typer.Argument(
+            help="The name of the visualization. This will be included in the task prompt and might help the LLM to interpret the images."
+        ),
+    ] = "space reclaiming icicle chart",
+    num_samples: int = 500,
+    images_per_sample: int = 2,
+):
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
     # Get a list of all image files in the folder
-    image_files = [file for file in os.listdir(folder_path) if file.endswith(".png")]
+    image_files = [file for file in os.listdir(input_folder) if file.endswith(".png")]
+    if len(image_files) < num_samples * (images_per_sample + 1):
+        raise ValueError("Not enough images in the input folder")
     print(f"Found {len(image_files)} images")
     random.shuffle(image_files)
 
     # Randomly select two images for each sample ("different" samples)
     samples = []
-    os.makedirs(output_path + "different", exist_ok=True)
-    for i in tqdm(range(NUM_SAMPPLES)):
-        if len(image_files) < IMAGES_PER_SAMPLE:
+    for i in tqdm(range(num_samples)):
+        if len(image_files) < images_per_sample:
             raise ValueError("not enough samples left")
-        random_images = [f"{folder_path}/{image_files.pop()}" for _ in range(IMAGES_PER_SAMPLE)]
-        sample_dir = f"{output_path}different/{i}"
+        random_images = [
+            f"{input_folder}/{image_files.pop()}" for _ in range(images_per_sample)
+        ]
+        sample_dir = f"{output_folder}/different/{i}"
         os.makedirs(sample_dir, exist_ok=True)
         [shutil.copy2(image, sample_dir) for image in random_images]
         random_images = [Image.open(f).convert("RGB") for f in random_images]
-        
-        samples.append(build_sample(random_images, same=False))
-        
+
+        samples.append(
+            build_sample(random_images, same=False, visualization=visualization)
+        )
+
     print("Different samples done")
     print(f"Images left: {len(image_files)}")
     # "same" samples
-    os.makedirs(output_path + "same", exist_ok=True)
-    for i in tqdm(range(NUM_SAMPPLES)):
-        random_image = f"{folder_path}/{image_files.pop()}"
-        sample_dir = f"{output_path}same/{i}"
+    for i in tqdm(range(num_samples)):
+        random_image = f"{input_folder}/{image_files.pop()}"
+        sample_dir = f"{output_folder}/same/{i}"
         os.makedirs(sample_dir, exist_ok=True)
 
-        images = create_contrastive_views(random_image)
+        images = create_contrastive_views(random_image, images_per_sample)
         [image.save(f"{sample_dir}/{i}.png") for i, image in enumerate(images)]
 
         samples.append(
-            build_sample(create_contrastive_views(random_image), same=True)
+            build_sample(
+                create_contrastive_views(random_image, images_per_sample),
+                same=True,
+                visualization=visualization,
+            )
         )
     print("Same samples done")
     # Write the samples to a JSONL file
     up_to = len(samples) // 10
-    with jsonlines.open(output_path + "contrastive_test.jsonl", "w") as writer:
+    with jsonlines.open(output_folder / "contrastive_test.jsonl", "w") as writer:
         for sample in samples[:up_to]:
             writer.write(sample)
     print("Done writing samples to contrastive_test.jsonl")
-    with jsonlines.open(output_path + "contrastive_train.jsonl", "w") as writer:
+    with jsonlines.open(output_folder / "contrastive_train.jsonl", "w") as writer:
         for sample in samples[up_to:]:
             writer.write(sample)
     print("Done writing samples to contrastive_train.jsonl")
+
+
+if __name__ == "__main__":
+    app()
