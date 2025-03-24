c29c6d7b5afa06b0eb4ba08f621d71ec66b037c3
Kilian Bartz <kibartz@proton.me>
2025-03-24 12:37:06 +0000

    updated eval_oai cli

--- eval/build_ideal_mac_results.py
+++ eval/build_ideal_mac_results.py
@@ -1,20 +1,22 @@
 import json
 from glob import glob
 from tqdm import tqdm
+from pathlib import Path
 
-BASEPATH = "../data/retrieval_queries"
-MODES = ["simple", "complex"]
-OUTPUT_PATH = "../data/eval_all"
 
-for mode in MODES:
+def convert(basepath: Path, mode: str, outpath: Path):
+    """
+    Writes the ideal MAC results for every arguement retrieval query of a given mode in a given basepath to a single json file.
+    :param basepath: Path to the directory containing the microtexts-retrieval-{mode} directories.
+    """
     res = {}
-    for f in tqdm(glob(f"{BASEPATH}/microtexts-retrieval-{mode}/*.json")):
+    for f in tqdm(glob(f"{basepath}/microtexts-retrieval-{mode}/*.json")):
         queryname = f.split("/")[-1].split(".")[0]
         with open(f, "r") as f:
             data = json.load(f)
             res[queryname] = list(
                 data["userdata"]["cbrEvaluations"][0]["ranking"].keys()
             )
-    with open(f"{OUTPUT_PATH}/ideal_mac_{mode}.json", "w") as f:
+    with open(outpath, "w") as f:
         json.dump(res, f, indent=4)
-        print(f"Saved {OUTPUT_PATH}/ideal_mac_{mode}.json")
+        print(f"Saved {outpath}")
--- eval/eval_oai.py
+++ eval/eval_oai.py
@@ -13,45 +13,173 @@ from tqdm import tqdm
 import copy
 import os
 import time
+import typer
+from typing_extensions import Annotated
+from enum import Enum
+from pathlib import Path
+
+app = typer.Typer()
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Evaluation script for vision retrieval"
-    )
-    parser.add_argument("--mode", default="complex", help="Evaluation mode")
-    parser.add_argument(
-        "--basepath", required=True, help="Base path for evaluation data"
-    )
-    parser.add_argument("--openai-model", required=True, help="OpenAI model identifier")
-    parser.add_argument(
-        "--queries-base-path", required=True, help="Base path for retrieval queries"
-    )
-    return parser.parse_args()
+class Mode(str, Enum):
+    SIMPLE = "simple"
+    COMPLEX = "complex"
 
-
-args = parse_args()
 
 load_dotenv()  # take environment variables from .env.
 
 client = OpenAI()
 IMG_PLACEHOLDER = "data:image/png;base64,"
-MODE = args.mode
-BASEPATH = args.basepath
-RANKING_SAVE_PATH = f"{BASEPATH}/results/oai/ranking_oai_epoch1_temp0_{MODE}.json"
-RESULTS_PATH = f"{BASEPATH}/results/oai/results_oai_epoch1_temp0_{MODE}.json"
-QUERY_IMAGES = f"{BASEPATH}/microtexts-retrieval-{MODE}/srip"
-MAC_RESULTS_PATH = f"{BASEPATH}/ideal_mac_{MODE}.json"
-
-OPENAI_MODEL = args.openai_model
-queries_texts = f"{args.queries_base_path}/microtexts-retrieval-{MODE}"
 
 
-res = {}
-ranking_oai = {}
-qrels = {}
-run = {}
-sum_tokens = 0
+@app.command()
+def eval_oai(
+    openai_model: Annotated[
+        str,
+        typer.Argument(help="The fine-tuned OpenAI model to be evaluated"),
+    ],
+    mode: Mode,
+    queries_path: Annotated[
+        Path,
+        typer.Argument(
+            help="This path should contain the queries as arguebuf-compatible files (e.g. JSON) as well as the corresponding images with the same name (e.g. graph1.png for graph1.json)."
+        ),
+    ],
+    casebase_images_path: Annotated[
+        Path,
+        typer.Argument(
+            help="This path should contain the images for the casebase argument graphs with the same name (e.g. case1.png for case1.json)."
+        ),
+    ],
+    mac_results_path: Path,
+    output_path: Path,
+):
+
+    ranking_path = output_path / "ranking.json"
+    results_path = output_path / "results.json"
+
+    res = {}
+    ranking_oai = {}
+    qrels = {}
+    run = {}
+    sum_tokens = 0
+    query_names = [
+        path.split("/")[-1].split(".")[0] for path in glob(f"{queries_path}/*.json")
+    ]
+    loaded_existing = os.path.exists(ranking_path)
+    # mac_results = build_mac_results(MAC_RESULTS_PATH)
+    mac_results = build_ideal_mac_results(mac_results_path)
+    print(f"Using these MAC results: {mac_results_path}")
+    request_durations = []
+    images_sent = []
+    if loaded_existing:
+        with open(ranking_path, "r") as f:
+            ranking_oai = json.load(f)
+            print("Loaded existing ranking")
+            sum_tokens = ranking_oai["sum_tokens"]
+            request_durations = ranking_oai["request_durations"]
+    else:
+        print(
+            f"No existing ranking found. Starting new evaluation with {len(query_names)} {mode} queries..."
+        )
+    for query_name in tqdm(query_names):
+        query_text = f"{queries_path}/{query_name}.json"
+        query_image = f"{queries_path}/{query_name}.png"
+
+        with open(query_text, "r") as f:
+            query_text = json.load(f)
+        reference_rankings = {
+            k.split("/")[1]: 4 - v
+            for k, v in query_text["userdata"]["cbrEvaluations"][0]["ranking"].items()
+        }
+        qrels[query_name] = reference_rankings
+        casebase_images = [
+            f"{casebase_images_path}/{n}.png" for n in mac_results[query_name]
+        ]
+        images_sent.append(casebase_images)
+        if loaded_existing and query_name in ranking_oai:
+            answer = ranking_oai[query_name]
+            print(f"{query_name}: Used existing ranking")
+        else:
+            prompt_messages = [
+                {
+                    "role": "system",
+                    "content": "You are an assistant that specializes in retrieving argument graphs based on their visualizations.",
+                },
+                {
+                    "role": "user",
+                    "content": f"Take a look at the following images in space reclaiming icicle chart visualization. Image 1 represents the query, images 2-{1 + len(reference_rankings.keys())} are retrieval candidates. Please rank all retrieval candidates (images 2-{1 + len(reference_rankings.keys())}) in descending order based on their similarity to the query image. If there were, for example, the three retrieval candidates images 2-4, image 3 having the highest similarity, image 2 the second highest and image 4 the lowest, you would output: 3,2,4.",
+                },
+                {"role": "user", "content": "This is image 1, the query"},
+            ]
+            prompt_messages.append(append_images(query_image))
+            prompt_messages.append(
+                {
+                    "role": "user",
+                    "content": f"The following are images 2-{1 + len(casebase_images)}, retrieval candidates:",
+                }
+            )
+            for img in casebase_images:
+                prompt_messages.append(append_images(img))
+            prompt_messages.append(
+                {
+                    "role": "user",
+                    "content": "The correct order of the retrieval images is:",
+                }
+            )
+            request_start = time.time()
+            completion = client.chat.completions.create(
+                model=openai_model,
+                messages=prompt_messages,
+                temperature=0.0,
+            )
+            request_durations.append(time.time() - request_start)
+            answer = completion.choices[0].message.content
+            sum_tokens += completion.usage.total_tokens
+        try:
+            keys = [key.split("/")[-1] for key in mac_results[query_name]]
+            order = [int(i) for i in answer.split(",")]
+            # assert len(set(order)) == len(
+            #     reference_rankings
+            # ), "Rankings must be unique and equal to the number of images"
+            # assert set(order) == set(
+            #     range(2, len(reference_rankings) + 2)
+            # ), "Rankings must be a permutation of 1 to N"
+        except ValueError:
+            order = []
+        simulated_sims = {
+            keys[idx - 2]: 1 - 0.1 * (pos + 1) for pos, idx in enumerate(order)
+        }
+        run[query_name] = simulated_sims
+        # stattdessen Evaluation(query)
+        ranking_oai[query_name] = answer
+        res[query_name] = {
+            "correctness_completeness": _correctness_completeness_single(
+                reference_rankings, simulated_sims, None
+            )
+        }
+    print(res)
+    ranking_oai["sum_tokens"] = sum_tokens
+    ranking_oai["request_durations"] = request_durations
+    ranking_oai["images_sent"] = images_sent
+    with open(ranking_path, "w") as f:
+        json.dump(ranking_oai, f)
+    print("Starting evaluation")
+    start = time.time()
+    run = Run(run)
+    evaluate(
+        Qrels(qrels),
+        run,
+        ["ndcg_burges", "ndcg", "map", "f1", "recall", "precision"],
+        return_mean=False,
+    )
+    results = as_dict(query_names, copy.deepcopy(run.mean_scores), qrels, run, None)
+    results["sum_tokens"] = sum_tokens
+    results["request_duration"] = sum(request_durations)
+    with open(results_path, "w") as f:
+        json.dump(results, f)
+    print(results)
+    print(f"Evaluation duration: {time.time() - start}")
 
 
 def as_dict(queries, results, qrels, run, k):
@@ -84,7 +212,7 @@ def build_mac_results(eval_json_path: str) -> dict[str, list[str]]:
     return out
 
 
-def build_ideal_mac_results(eval_json_path: str) -> dict[str, list[str]]:
+def build_ideal_mac_results(eval_json_path: Path) -> dict[str, list[str]]:
     with open(eval_json_path, "r") as f:
         return json.load(f)
 
@@ -101,121 +229,3 @@ def append_images(image) -> dict:
             }
         ],
     }
-
-
-query_names = [
-    path.split("/")[-1].split(".")[0] for path in glob(f"{queries_texts}/*.json")
-]
-loaded_existing = os.path.exists(RANKING_SAVE_PATH)
-# mac_results = build_mac_results(MAC_RESULTS_PATH)
-mac_results = build_ideal_mac_results(MAC_RESULTS_PATH)
-print(f"Using these MAC results: {MAC_RESULTS_PATH}")
-request_durations = []
-images_sent = []
-if loaded_existing:
-    with open(RANKING_SAVE_PATH, "r") as f:
-        ranking_oai = json.load(f)
-        print("Loaded existing ranking")
-        sum_tokens = ranking_oai["sum_tokens"]
-        request_durations = ranking_oai["request_durations"]
-else:
-    print(
-        f"No existing ranking found. Starting new evaluation with {len(query_names)} {MODE} queries..."
-    )
-for query_name in tqdm(query_names):
-    query_text = f"{queries_texts}/{query_name}.json"
-    query_image = f"{QUERY_IMAGES}/{query_name}.png"
-
-    with open(query_text, "r") as f:
-        query_text = json.load(f)
-    reference_rankings = {
-        k.split("/")[1]: 4 - v
-        for k, v in query_text["userdata"]["cbrEvaluations"][0]["ranking"].items()
-    }
-    qrels[query_name] = reference_rankings
-    casebase_path = f"{BASEPATH}/casebase/srip"
-    casebase_images = [f"{casebase_path}/{n}.png" for n in mac_results[query_name]]
-    images_sent.append(casebase_images)
-    if loaded_existing and query_name in ranking_oai:
-        answer = ranking_oai[query_name]
-        print(f"{query_name}: Used existing ranking")
-    else:
-        prompt_messages = [
-            {
-                "role": "system",
-                "content": "You are an assistant that specializes in retrieving argument graphs based on their visualizations.",
-            },
-            {
-                "role": "user",
-                "content": f"Take a look at the following images in space reclaiming icicle chart visualization. Image 1 represents the query, images 2-{1 + len(reference_rankings.keys())} are retrieval candidates. Please rank all retrieval candidates (images 2-{1 + len(reference_rankings.keys())}) in descending order based on their similarity to the query image. If there were, for example, the three retrieval candidates images 2-4, image 3 having the highest similarity, image 2 the second highest and image 4 the lowest, you would output: 3,2,4.",
-            },
-            {"role": "user", "content": "This is image 1, the query"},
-        ]
-        prompt_messages.append(append_images(query_image))
-        prompt_messages.append(
-            {
-                "role": "user",
-                "content": f"The following are images 2-{1 + len(casebase_images)}, retrieval candidates:",
-            }
-        )
-        for img in casebase_images:
-            prompt_messages.append(append_images(img))
-        prompt_messages.append(
-            {
-                "role": "user",
-                "content": "The correct order of the retrieval images is:",
-            }
-        )
-        request_start = time.time()
-        completion = client.chat.completions.create(
-            model=OPENAI_MODEL,
-            messages=prompt_messages,
-            temperature=0.0,
-        )
-        request_durations.append(time.time() - request_start)
-        answer = completion.choices[0].message.content
-        sum_tokens += completion.usage.total_tokens
-    try:
-        keys = [key.split("/")[-1] for key in mac_results[query_name]]
-        order = [int(i) for i in answer.split(",")]
-        # assert len(set(order)) == len(
-        #     reference_rankings
-        # ), "Rankings must be unique and equal to the number of images"
-        # assert set(order) == set(
-        #     range(2, len(reference_rankings) + 2)
-        # ), "Rankings must be a permutation of 1 to N"
-    except ValueError:
-        order = []
-    simulated_sims = {
-        keys[idx - 2]: 1 - 0.1 * (pos + 1) for pos, idx in enumerate(order)
-    }
-    run[query_name] = simulated_sims
-    # stattdessen Evaluation(query)
-    ranking_oai[query_name] = answer
-    res[query_name] = {
-        "correctness_completeness": _correctness_completeness_single(
-            reference_rankings, simulated_sims, None
-        )
-    }
-print(res)
-ranking_oai["sum_tokens"] = sum_tokens
-ranking_oai["request_durations"] = request_durations
-ranking_oai["images_sent"] = images_sent
-with open(RANKING_SAVE_PATH, "w") as f:
-    json.dump(ranking_oai, f)
-print("Starting evaluation")
-start = time.time()
-run = Run(run)
-evaluate(
-    Qrels(qrels),
-    run,
-    ["ndcg_burges", "ndcg", "map", "f1", "recall", "precision"],
-    return_mean=False,
-)
-results = as_dict(query_names, copy.deepcopy(run.mean_scores), qrels, run, None)
-results["sum_tokens"] = sum_tokens
-results["request_duration"] = sum(request_durations)
-with open(RESULTS_PATH, "w") as f:
-    json.dump(results, f)
-print(results)
-print(f"Evaluation duration: {time.time() - start}")
